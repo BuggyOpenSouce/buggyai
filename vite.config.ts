50263d78916d02d723d40ddae5e16336f6841754
BuggyOpenSouce <roverthearbiterone@gmail.com>
2025-06-01 15:45:15 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
+  base: './', // Electron build için eklendi
   plugins: [
     react(),
     ViteImageOptimizer({
